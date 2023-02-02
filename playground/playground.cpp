2c76de5a1e52e6572cb7b805b33724e00a8798ee
W298 <lutca1320@gmail.com>
2023-02-02 16:41:07 +0900

    Keyboard & Mouse input

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -5,59 +5,83 @@
 #include <GLFW/glfw3.h>
 
 #include <common/shader.hpp>
+#include <common/texture.hpp>
 
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/transform.hpp>
 #include <glm/glm.hpp>
 using namespace glm;
 
-GLuint loadBMP_custom(const char* imgPath) {
-    unsigned char header[54];
-    unsigned int dataFirstPos;
-    unsigned int width, height;
-    unsigned int imageSize;
-    unsigned char* data;
+mat4 computeMatricesFromInputs(GLFWwindow* window) {
+    static double lastTime = glfwGetTime();
+    static float initialFOV = 50.0f;
+    static float FOV = initialFOV;
 
-    FILE* file = fopen(imgPath, "rb");
-    if (!file) {
-        printf("Unable to open image\n");
-        return 0;
+    static vec3 position = vec3(2, 2, 5);
+    static float horizontalAngle = 3.14f;
+    static float verticalAngle = 0.0f;
+
+    float speed = 3.0f; // Move speed with keyboard
+    float mouseSpeed = 0.01f; // Move speed with mouse
+
+    double xpos, ypos; // mouse x, y pos
+    int width, height;
+
+    double currentTime = glfwGetTime();
+    float deltaTime = float(currentTime - lastTime);
+
+    glfwGetWindowSize(window, &width, &height);
+    glfwGetCursorPos(window, &xpos, &ypos);
+
+    // Return mouse cursor to center
+    glfwSetCursorPos(window, width / 2, height / 2);
+
+    // Add delta angle
+    horizontalAngle += mouseSpeed * deltaTime * (width / 2 - xpos);
+    verticalAngle = std::min(3.14f / 4, std::max(-3.14f / 4, verticalAngle + mouseSpeed * deltaTime * float(height / 2 - ypos)));
+
+    // Forward vector (Spherical => Orthogonal)
+    vec3 direction(
+        cos(verticalAngle) * sin(horizontalAngle),
+        sin(verticalAngle),
+        cos(verticalAngle) * cos(horizontalAngle)
+    );
+
+    // Right vector
+    vec3 right(
+        sin(horizontalAngle - 3.14f / 2),
+        0,
+        cos(horizontalAngle - 3.14f / 2)
+    );
+
+    // Up vector
+    vec3 up = cross(right, direction);
+
+    // Keyboard input handle
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        position += direction * deltaTime * speed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        position -= direction * deltaTime * speed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+        position += right * deltaTime * speed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+        position -= right * deltaTime * speed;
     }
 
-    if (fread(header, 1, 54, file) != 54) {
-        printf("Not a correct BMP image\n");
-        return 0;
-    }
+    // Mouse wheel callback (FOV)
+    glfwSetScrollCallback(window, [](GLFWwindow* window, double xOffset, double yOffset) {
+        FOV -= 5 * yOffset;
+    });
 
-    if (header[0] != 'B' || header[1] != 'M') {
-        printf("Not a correct BMP image\n");
-        return 0;
-    }
+    lastTime = currentTime;
 
-    dataFirstPos = *(int*)&(header[0x0A]);
-    imageSize = *(int*)&(header[0x22]);
-    width = *(int*)&(header[0x12]);
-    height = *(int*)&(header[0x16]);
+    // Calculate matrix
+    mat4 projectionMat = perspective(radians(FOV), (float)width / (float)height, 0.1f, 100.0f);
+    mat4 viewMat = lookAt(position, position + direction, up);
 
-    if (imageSize == 0) imageSize = width * height * 3;
-    if (dataFirstPos == 0) dataFirstPos = 54;
-
-    data = new unsigned char[imageSize];
-    fread(data, 1, imageSize, file);
-    fclose(file);
-
-    // Bind data to opengl
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    return textureID;
+    return projectionMat * viewMat;
 }
 
 int main()
@@ -104,13 +128,6 @@ int main()
 
     // Compile GLSL program from shaders
     GLuint programID = LoadShaders("shaders/SimpleVertexShader.vertexshader", "shaders/SimpleFragmentShader.fragmentshader");
-
-    // Create MVP Matrix
-    mat4 projectionMat = perspective(radians(45.0f), width / height, 0.1f, 100.0f);
-    mat4 viewMat = lookAt(vec3(4, 3, -3), vec3(0, 0, 0), vec3(0, 1, 0));
-    mat4 modelMat = mat4(1.0f);
-
-    mat4 mvpMat = projectionMat * viewMat * modelMat;
 
     // Get a handle for MVP uniform in shader
     GLuint matrixID = glGetUniformLocation(programID, "MVP");
@@ -211,8 +228,9 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
 
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST); // Enable Depth test
     glDepthFunc(GL_LESS);
+    glEnable(GL_CULL_FACE); // Enable Culling
 
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f); // Clear color to dark blue
 
@@ -249,6 +267,11 @@ int main()
             0,
             (void*)0
         );
+
+        // Calculate MVP Matrix each frame
+        mat4 pvMat = computeMatricesFromInputs(window);
+        mat4 modelMat = mat4(1.0f);
+        mat4 mvpMat = pvMat * modelMat;
 
         glUniformMatrix4fv(matrixID, 1, GL_FALSE, &mvpMat[0][0]); // Send MVP Matrix to shader
         glDrawArrays(GL_TRIANGLES, 0, 12*3); // Draw Cube
